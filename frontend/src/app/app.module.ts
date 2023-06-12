18908cdf9327c7409b395cbd5eb2fbb1720ec8c7
shmato4ek <58270694+shmato4ek@users.noreply.github.com>
2023-06-12 10:10:52 +0300

    Added frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BaseComponent } from './modules/workspace/base/base.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BaseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
